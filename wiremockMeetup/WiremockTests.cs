477e9653d7950416f63d43c995590eb6ac052e88
Richard.Winterhalder <richie676@gmail.com>
2020-02-03 15:52:42 +0100

    init commit, project setup

---
+++ wiremockMeetup/WiremockTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WireMock.Matchers;
+using WireMock.Server;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using System.Threading;
+
+namespace wiremockMeetup
+{
+    [TestClass]
+    public class WiremockTests
+    {
+        [TestMethod]
+        public void WiremockSetup()
+        {
+            var wiremockServer = FluentMockServer.Start(8025);
+
+            var wildcard = new WildcardMatcher("*");
+
+            wiremockServer
+              .Given(
+              Request.Create().WithPath("/example/api")
+              .UsingPost()
+              .WithParam("user", wildcard)
+              .UsingPost()
+              )
+              .AtPriority(1)
+              .RespondWith(
+                Response.Create()
+                  .WithStatusCode(200)
+                  .WithHeader("access-control-allow-origin", "*")
+                  .WithHeader("Content-Type", "text/json")
+                  .WithHeader("Hallo", "World")
+              );
+
+            Thread.Sleep(15_000);
+            wiremockServer.Stop();
+        }
+    }
+}
