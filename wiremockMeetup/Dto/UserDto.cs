c503e817b42ed857751bf8f27a5cec6448de7274
Richard.Winterhalder <richie676@gmail.com>
2020-02-09 19:23:52 +0100

    added serilog added flurl

---
+++ wiremockMeetup/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wiremockMeetup.Dto
+{
+    public class UserDto
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+    }
+}
--- wiremockMeetup/WiremockTests.cs
+++ wiremockMeetup/WiremockTests.cs
@@ -4,37 +4,58 @@ using WireMock.Server;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using System.Threading;
+using Flurl.Http;
+using wiremockMeetup.Dto;
+using Serilog;
+using Serilog.Core;
 
 namespace wiremockMeetup
 {
     [TestClass]
     public class WiremockTests
     {
+        ILogger log;
+
+        [TestInitialize]
+        public void Setup() 
+        {
+            log = Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console()
+                        .CreateLogger();
+        }
+
         [TestMethod]
         public void WiremockSetup()
         {
-            var wiremockServer = FluentMockServer.Start(80);
-
-            var wildcard = new WildcardMatcher("*");
+            var wiremockServer = FluentMockServer.Start(5000);
 
             wiremockServer
               .Given(
-              Request.Create().WithPath("/example/api")
-              .UsingPost()
-              .WithParam("user", wildcard)
-              .UsingPost()
+              Request.Create().WithPath("/api/user")
+              .UsingGet()
               )
-              .AtPriority(1)
+              .AtPriority(100)
               .RespondWith(
                 Response.Create()
                   .WithStatusCode(200)
-                  .WithHeader("access-control-allow-origin", "*")
-                  .WithHeader("Content-Type", "text/json")
-                  .WithHeader("Hallo", "World")
+                  .WithBodyAsJson(new UserDto() { firstName = "Walter", lastName = "Wiremock", userName = "WireWalter"})
               );
 
-            Thread.Sleep(15_000);
+            string urlLocal = "http://localhost:5000/api/user";
+            UserDto resp = urlLocal.GetJsonAsync<UserDto>().Result;
+            log.Information("User: {@resp}", resp);
+
             wiremockServer.Stop();
+        }
+
+        [TestMethod]
+        public void GetUserShoudReturnUserMax()
+        {
+            string urlLocal = "http://localhost:5000/api/user";
+            string urlAzure = "http://playgroundapimeetup.azurewebsites.net/api/user";
+            UserDto resp = urlLocal.GetJsonAsync<UserDto>().Result;
+
+            log.Information("User: {@resp}", resp);
         }
     }
 }
